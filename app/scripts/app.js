a861a0ebc5d920ecb1437661a0865fe620353b69
MorganeLeBris <morganebris@gmail.com>
2017-05-14 13:59:34 -0400

    base

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularMobileApp
+ * @description
+ * # angularMobileApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularMobileApp', []);
